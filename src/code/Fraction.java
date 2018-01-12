238a2ceef0dc3247ceaf15cd5e23579f73f87ec7
gwelch43 <garrettwelch39@yahoo.com>
2018-01-12 09:17:53 -0600

    Created a simplify method a gcf method and changed the constructor to diallow a denominator of zero.

--- src/code/Fraction.java
+++ src/code/Fraction.java
@@ -1,13 +1,23 @@
 package code;
 
+import java.util.IllegalFormatException;
+
 public class Fraction
 {
     private int num, denom;
 
     public Fraction(int num, int denom)
     {
-        this.num = num;
-        this.denom = denom;
+        if (denom ==0)
+        {
+            throw new IllegalArgumentException("Fractions can't have a zero denominator");
+        }
+        else
+        {
+            this.num = num;
+            this.denom = denom;
+            simplify();
+        }
     }
 
     public Fraction(int num)
@@ -27,4 +37,39 @@ public class Fraction
         return num + " / " + denom;
 
     }
+    public Fraction multiply(Fraction f)
+    {
+        return new Fraction ( num*f.num, denom*f.denom);
+    }
+    public Fraction divide(Fraction f)
+    {
+        return new Fraction ( num*f.denom, denom*f.num);
+    }
+    public static int gcf(int a, int b)
+    {
+        int min = Math.min(a,b);
+        for (int i = min; i > 0; i--)
+        {
+            if (a%i == 0 && b%i == 0) return i;
+        }
+        return 1;
+    }
+    private void simplify()
+    {
+        /*int min = Math.min(num,denom);
+        for (int i = min; i > 0; i--)
+        {
+            if (num%i == 0 && denom%i == 0)
+            {
+                num /= i;
+                denom /= i;
+                return;
+            }
+        }
+        */
+        int factor = gcf(num,denom);
+        num /= factor;
+        denom /= factor;
+    }
+
 }
--- src/code/Runner.java
+++ src/code/Runner.java
@@ -3,11 +3,20 @@ package code;
 public class Runner
 {
     public static void main(String[] args)
-    {
-        Fraction myFraction = new Fraction(  2, 3);
-        Fraction otherFraction = new Fraction( 5);
-        Fraction a = myFraction;
 
+
+    {
+        /*Fraction f1 = null;
+        try
+        {
+             f1 = new Fraction(3, 0);
+        } catch (Exception e) {
+            System.out.println("Fractions need a non-zero denominator");
+        }
+        */
+        Fraction myFraction = new Fraction( 12,4);
         System.out.println(myFraction);
+
     }
+
 }
